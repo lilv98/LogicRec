d9da5d63909f97b1572432e4c677b4ccd4d8469c
lilv98 <zhenwei.tang@kaust.edu.sa>
2022-12-29 19:20:21 +0300

    lqa_datloader_done

--- code/logicrec.py
+++ code/logicrec.py
@@ -121,7 +121,7 @@ class RSDatasetTrain(torch.utils.data.Dataset):
         neg_item = torch.tensor(np.random.choice(self.N_item, self.num_ng // 2)).unsqueeze(dim=1)
         neg_i = torch.cat([user.unsqueeze(dim=0).expand(self.num_ng // 2, 1), neg_item], dim=1)
         neg_u = torch.cat([neg_user, item.unsqueeze(dim=0).expand(self.num_ng // 2, 1)], dim=1)
-        sample = torch.cat([torch.tensor([user, item]).unsqueeze(0), neg_i, neg_u], dim=0)
+        sample = torch.cat([torch.tensor([user, item]).unsqueeze(dim=0), neg_i, neg_u], dim=0)
         return sample
 
 class RSDatasetTest(torch.utils.data.Dataset):
@@ -146,6 +146,35 @@ class RSDatasetTest(torch.utils.data.Dataset):
         users = pos[0].unsqueeze(dim=0).expand(N_item, 1)
         items = torch.arange(N_item).unsqueeze(dim=1)
         return torch.cat([users, items], dim=-1), pos
+
+class LQADatasetTrain(torch.utils.data.Dataset):
+    def __init__(self, N_item, data, cfg):
+        super().__init__()
+        self.N_item = N_item
+        self.num_ng = cfg.num_ng
+        self.data = self._get_data(data)
+
+    def _get_data(self, data):
+        ret = []
+        for query in data:
+            query_as_list = list(query)
+            items = data[query]
+            for item in items:
+                query_as_list.extend([item])
+                ret.append(query_as_list)
+                query_as_list = list(query)
+        return torch.tensor(ret)
+
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        pos = self.data[idx]
+        neg_answers = torch.tensor(np.random.choice(self.N_item, self.num_ng)).unsqueeze(dim=1)
+        query = pos[:-1].expand(self.num_ng, -1)
+        negs = torch.cat([query, neg_answers], dim=1)
+        sample = torch.cat([pos.unsqueeze(dim=0), negs], dim=0)
+        return sample
 
 class RSModel(torch.nn.Module):
     def __init__(self, N_user, N_item, cfg):
@@ -238,7 +267,7 @@ def parse_args(args=None):
     parser.add_argument('--wd', default=1e-5, type=int)
     parser.add_argument('--max_epochs', default=1000, type=int)
     parser.add_argument('--rs_base_model', default='BPRMF', type=str)
-    parser.add_argument('--num_workers', default=8, type=int)
+    parser.add_argument('--num_workers', default=4, type=int)
     parser.add_argument('--bs', default=1024, type=int)
     parser.add_argument('--verbose', default=1, type=int)
     parser.add_argument('--gpu', default=0, type=int)
@@ -257,9 +286,19 @@ if __name__ == '__main__':
     N_rel, N_item, N_ent, N_user, kg, train_dict, test_dict = read_data(input_path)
     
     train_1p, test_1p = load_obj(input_path + '/input/1p_train.pkl'), load_obj(input_path + '/input/1p_test.pkl')
+    train_2p, test_2p = load_obj(input_path + '/input/2p_train.pkl'), load_obj(input_path + '/input/2p_test.pkl')
+    train_3p, test_3p = load_obj(input_path + '/input/3p_train.pkl'), load_obj(input_path + '/input/3p_test.pkl')
+    train_2i, test_2i = load_obj(input_path + '/input/2i_train.pkl'), load_obj(input_path + '/input/2i_test.pkl')
+    train_3i, test_3i = load_obj(input_path + '/input/3i_train.pkl'), load_obj(input_path + '/input/3i_test.pkl')
+    
     kge_dataset = KGEDataset(N_ent, kg, cfg)
     rs_dataset_train = RSDatasetTrain(N_user, N_item, train_dict, cfg)
     rs_dataset_test = RSDatasetTest(N_item, test_dict)
+    lqa_dataset_1p_train = LQADatasetTrain(N_item, train_1p, cfg)
+    lqa_dataset_2p_train = LQADatasetTrain(N_item, train_2p, cfg)
+    lqa_dataset_3p_train = LQADatasetTrain(N_item, train_3p, cfg)
+    lqa_dataset_2i_train = LQADatasetTrain(N_item, train_2i, cfg)
+    lqa_dataset_3i_train = LQADatasetTrain(N_item, train_3i, cfg)
 
     kge_dataloader = torch.utils.data.DataLoader(dataset=kge_dataset,
                                                 batch_size=cfg.bs,
@@ -276,6 +315,31 @@ if __name__ == '__main__':
                                                 num_workers=cfg.num_workers,
                                                 shuffle=True,
                                                 drop_last=True)
+    lqa_dataloader_1p_train = torch.utils.data.DataLoader(dataset=lqa_dataset_1p_train,
+                                                batch_size=cfg.bs,
+                                                num_workers=cfg.num_workers,
+                                                shuffle=True,
+                                                drop_last=True)
+    lqa_dataloader_2p_train = torch.utils.data.DataLoader(dataset=lqa_dataset_2p_train,
+                                                batch_size=cfg.bs,
+                                                num_workers=cfg.num_workers,
+                                                shuffle=True,
+                                                drop_last=True)
+    lqa_dataloader_3p_train = torch.utils.data.DataLoader(dataset=lqa_dataset_3p_train,
+                                                batch_size=cfg.bs,
+                                                num_workers=cfg.num_workers,
+                                                shuffle=True,
+                                                drop_last=True)
+    lqa_dataloader_2i_train = torch.utils.data.DataLoader(dataset=lqa_dataset_2i_train,
+                                                batch_size=cfg.bs,
+                                                num_workers=cfg.num_workers,
+                                                shuffle=True,
+                                                drop_last=True)
+    lqa_dataloader_3i_train = torch.utils.data.DataLoader(dataset=lqa_dataset_3i_train,
+                                                batch_size=cfg.bs,
+                                                num_workers=cfg.num_workers,
+                                                shuffle=True,
+                                                drop_last=True)
     
     model_rs = RSModel(N_user, N_item, cfg)
     model_rs = model_rs.to(device)
@@ -284,6 +348,11 @@ if __name__ == '__main__':
         kge_dataloader = tqdm.tqdm(kge_dataloader)
         rs_dataloader_train = tqdm.tqdm(rs_dataloader_train)
         rs_dataloader_test = tqdm.tqdm(rs_dataloader_test)
+        lqa_dataloader_1p_train = tqdm.tqdm(lqa_dataloader_1p_train)
+        lqa_dataloader_2p_train = tqdm.tqdm(lqa_dataloader_2p_train)
+        lqa_dataloader_3p_train = tqdm.tqdm(lqa_dataloader_3p_train)
+        lqa_dataloader_2i_train = tqdm.tqdm(lqa_dataloader_2i_train)
+        lqa_dataloader_3i_train = tqdm.tqdm(lqa_dataloader_3i_train)
     
     optimizer_rs = torch.optim.Adam(model_rs.parameters(), lr=cfg.lr, weight_decay=cfg.wd)
     
@@ -293,12 +362,25 @@ if __name__ == '__main__':
         print(f'Common -- Epoch {epoch + 1}:')
         model_rs.train()
         avg_loss = []
-        # for batch in zip(kge_dataloader, rs_dataloader_train):
-        #     batch_rs = batch[1].to(device)
-        #     loss_rs = model_rs.get_loss(batch_rs)
-        for batch in rs_dataloader_train:
-            batch = batch.to(device)
-            loss_rs = model_rs.get_loss(batch)
+        for batch in zip(kge_dataloader, 
+                         rs_dataloader_train,
+                         lqa_dataloader_1p_train,
+                         lqa_dataloader_2p_train,
+                         lqa_dataloader_3p_train,
+                         lqa_dataloader_2i_train,
+                         lqa_dataloader_3i_train
+                         ):
+            batch_kge = batch[0].to(device)
+            batch_rs = batch[1].to(device)
+            batch_1p = batch[2].to(device)
+            batch_2p = batch[3].to(device)
+            batch_3p = batch[4].to(device)
+            batch_2i = batch[5].to(device)
+            batch_3i = batch[6].to(device)
+            loss_rs = model_rs.get_loss(batch_rs)
+        # for batch in rs_dataloader_train:
+        #     batch = batch.to(device)
+        #     loss_rs = model_rs.get_loss(batch)
             optimizer_rs.zero_grad()
             loss_rs.backward()
             optimizer_rs.step()
